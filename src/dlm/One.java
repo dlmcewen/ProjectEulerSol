1278ba835b7933376c1623ce240ab900bc0a4d27
dlmcewen <dl.mcewen@yahoo.com>
2014-06-06 17:43:22 -0400

    2nd Commit

--- src/dlm/One.java
+++ src/dlm/One.java
@@ -1,9 +1,30 @@
 package dlm;
 
+/*
+ * Problem 1 - Project Euler
+ * 
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+
+/**
+ * @author David McEwen
+ *
+ * @returns Answer to #1
+ */
 public class One {
 
 	public static void main(String[] args) {
-		System.out.println("test");
+		
+		int sum = 0;
+		
+		for(int i=0; i<1000; i++)	{
+			
+			if (i%3==0 || i%5==0)	{
+				sum += i;
+			}
+		}
+		System.out.println("Sum = " + sum);
 	}
 
 }
